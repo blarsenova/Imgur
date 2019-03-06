343b1cc3ca9fb90145ac6d66f21667ba1d8595ac
Baktygul <blarsenova@gmail.com>
2019-03-06 15:39:34 -0600

    commiting codes

---
+++ src/test/java/Imgur_WChrome.java
@@ -0,0 +1,96 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Imgur_WChrome {
+    WebDriver driver;
+
+
+    @BeforeClass
+    public void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+
+
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://imgur.com");
+
+    }
+
+    @Test
+    public void uploadWSelenium() throws InterruptedException {
+
+        /*New Post:
+        Make sure u can upload an image and it takes you
+        to that page after
+         */
+
+
+        driver.findElement(By.linkText("New post")).click();
+
+        WebElement upload = driver.findElement(By.xpath("//input[@type='file']"));
+        Thread.sleep(2000);
+        upload.sendKeys("/Users/baktygullarsenova/Downloads/spring.jpg");
+
+
+    }
+
+
+    @Test
+    public void search() throws Exception {
+        /*Search:
+        Make sure u can search
+        & the results are tied up
+        to your query
+         */
+
+        //driver.get("http://imgur.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("flowers" + Keys.ENTER);
+        Thread.sleep(2000);
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("flowers"));
+    }
+
+    @Test
+    public void randomMode() throws InterruptedException {
+    /* Random mode function:
+    make sure random button works
+    and takes you
+    to the new page successfully
+     */
+
+        //Select dropdrowns=new Select(driver.findElement(By.className("Dropdown-title")));
+        driver.findElement(By.xpath("//div[@class='Dropdown sort Dropdown--upper']")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//div[@class='Dropdown-list']/div[3]")).click();
+
+
+
+
+    }
+
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+         driver.quit();
+
+
+    }
+}
